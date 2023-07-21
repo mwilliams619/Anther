cd95e78b4d243289365232fe38ec25eef44d6c2b
Matt Williams <96218413+mw-hphi@users.noreply.github.com>
2023-07-21 00:51:06 -0500

    Testing staged changes

--- Anther/models.py
+++ Anther/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
-class Playlist(models.Model):
-    name = models.CharField(max_length=80)
-    songs = models.CharField
+class SongProps(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+    danceability = models.IntegerField()
+    energy = models.IntegerField()
+    mode = models.IntegerField()
+    valence = models.IntegerField()
+    tempo = models.IntegerField()
+    uri = models.TextField()
+    key = models.IntegerField()
+    popularity = models.IntegerField()
+    genre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
