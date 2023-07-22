5946d60a5e11cf5b6c00ccbc9e5f2945cc997ff1
Matt Williams <96218413+mw-hphi@users.noreply.github.com>
2023-07-22 10:19:49 -0500

    gitignore changes

---
+++ anther_core/localsettings.py
@@ -0,0 +1,12 @@
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Anther_1',
+        'USER': 'mattwilliams',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 5433
+    }
+}
